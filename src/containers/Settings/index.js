ede67ca346e437cc586670903ce0ab4ce10fb7b6
reactivegeeks <software.dev0218@gmail.com>
2018-11-25 23:40:53 +0200

    add tabbar

--- src/containers/Settings/index.js
+++ src/containers/Settings/index.js
@@ -4,14 +4,42 @@ import { connect } from 'react-redux';
 
 import ReactTable from 'react-table';
 import PageTitle from 'components/PageTitle';
+import TabBar from 'components/UI/TabBar';
+import Tab from 'components/UI/Tab';
 import Button from 'components/UI/Button';
 import styles from './styles.module.scss';
 
+const tabs = {
+  _1: 'Admins',
+  _2: 'Charts',
+};
+
 class Settings extends Component {
+  state = {
+    activeTab: tabs._1,
+  }
+
+  handleTabClick = tab => {
+    this.setState({ activeTab: tab });
+  }
+
   render() {
+    const { activeTab } = this.state;
     return (
       <div>
         <PageTitle title="Settings" />
+        <TabBar>
+          <Tab
+            active={activeTab === tabs._1}
+            title={tabs._1}
+            onClick={() => this.handleTabClick(tabs._1)}
+          />
+          <Tab
+            active={activeTab === tabs._2}
+            title={tabs._2}
+            onClick={() => this.handleTabClick(tabs._2)}
+          />
+        </TabBar>
         <Button onClick={() => alert('Hi')}>Add admin</Button>
       </div>
     );
