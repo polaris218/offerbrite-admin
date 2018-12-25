d76b0cc2ffb59649212ea754681c6a05e5c619c8
reactivegeeks <software.dev0218@gmail.com>
2018-12-25 10:07:40 +0200

    update env

--- src/index.js
+++ src/index.js
@@ -15,6 +15,9 @@ import 'index.scss';
 const middleware = [reduxThunk, logger];
 
 // TODO: turn off logger in production mode
+if (process.env.NODE_ENV === 'development') {
+  middleware.push(logger);
+}
 
 export const store = createStore(reducers, {}, applyMiddleware(...middleware));
 
